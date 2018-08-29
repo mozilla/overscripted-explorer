a707fcff1e0e686afa0f257ba87ae18c5f5b1c28
Sarah Bird <birdsarah@mozilla.com>
2018-08-29 00:52:02 -0500

    Working

--- text_search/main.py
+++ text_search/main.py
@@ -2,6 +2,8 @@ import os
 
 from concurrent.futures import ThreadPoolExecutor
 from functools import partial
+from urllib.parse import urlparse
+
 
 from bokeh.document import without_document_lock
 from bokeh.io import curdoc
@@ -19,6 +21,9 @@ from pyspark import SparkContext, SQLContext
 from pyspark.sql.functions import udf
 from tornado.gen import coroutine
 
+
+
+
 ###
 # Initial setup
 ###
@@ -26,7 +31,7 @@ from tornado.gen import coroutine
 DATA_DIR = os.environ.get('DATA_DIR')
 APP_DIR = os.path.dirname(__file__)
 DATA_FILE = os.path.join(DATA_DIR, 'clean.parquet')
-EXECUTOR = ThreadPoolExecutor(max_workers=1)
+EXECUTOR = ThreadPoolExecutor(max_workers=4)
 
 doc = curdoc()
 sc = SparkContext.getOrCreate()
@@ -95,10 +100,10 @@ def start_apply():
 def update_results(
     total_count,
     filtered_count,
-    script_url_n,
-    script_url_se,
-    location_n,
-    location_se,
+    # script_url_n,
+    # script_url_se,
+    # location_n,
+    # location_se,
     script_url_nl_n,
     script_url_nl_se,
     location_nl_n,
@@ -114,12 +119,12 @@ def update_results(
         sample_n_rows=pr(total_count),
         filtered_n_rows=pr(filtered_count),
         percent_row=pc(filtered_count / total_count),
-        script_url_n=pr(script_url_n),
-        script_url_se=pr(script_url_se),
-        script_url_pc=pc(script_url_se / script_url_n),
-        location_n=pr(location_n),
-        location_se=pr(location_se),
-        location_pc=pc(location_se / location_n),
+        # script_url_n=pr(script_url_n),
+        # script_url_se=pr(script_url_se),
+        # script_url_pc=pc(script_url_se / script_url_n),
+        # location_n=pr(location_n),
+        # location_se=pr(location_se),
+        # location_pc=pc(location_se / location_n),
         script_url_nl_n=pr(script_url_nl_n),
         script_url_nl_se=pr(script_url_nl_se),
         script_url_nl_pc=pc(script_url_nl_se / script_url_nl_n),
@@ -132,25 +137,48 @@ def update_results(
     apply_button.disabled = False
 
 
+def do_spark_computation(text_value):
+    def get_netloc(x):
+        p = urlparse(x)
+        return f'{p.netloc}'
+    get_netloc_udf = udf(get_netloc)
+
+    df = spark.read.parquet(DATA_FILE)
+    frac = sample_frac.value / 100
+    sample = df.sample(False, frac)
+    sample = sample.withColumn(
+        'script_url_nl', get_netloc_udf(sample.script_url)
+    ).withColumn(
+        'location_nl', get_netloc_udf(sample.location)
+    )
+    filtered = sample.where(df[column_to_look_in.value].contains(text_value))
+    total_count = sample.count()
+    filtered_count = filtered.count()
+    # script_url_n = sample.select('script_url').distinct().count()
+    # script_url_se = filtered.select('script_url').distinct().count()
+    # location_n = sample.select('location').distinct().count()
+    # location_se = filtered.select('location').distinct().count()
+    script_url_nl_n = sample.select('script_url_nl').distinct().count()
+    script_url_nl_se = filtered.select('script_url_nl').distinct().count()
+    location_nl_n = sample.select('location_nl').distinct().count()
+    location_nl_se = filtered.select('location_nl').distinct().count()
+    return (total_count, filtered_count, script_url_nl_n, script_url_nl_se, location_nl_n, location_nl_se)
+
+
 @coroutine
 @without_document_lock
 def get_new_data():
     doc.add_next_tick_callback(start_apply)
-    df = spark.read.parquet(DATA_FILE)
-    frac = sample_frac.value / 100
-    sample = df.sample(False, frac)
-    filtered = sample.where(df[column_to_look_in.value].contains(text_to_find.value))
-
-    total_count = yield EXECUTOR.submit(sample.count)
-    filtered_count = yield EXECUTOR.submit(filtered.count)
+    results = yield EXECUTOR.submit(do_spark_computation, text_to_find.value)
+    total_count, filtered_count, script_url_nl_n, script_url_nl_se, location_nl_n, location_nl_se = results
     doc.add_next_tick_callback(partial(
         update_results,
         total_count,
         filtered_count,
-        script_url_n=script_url_n,
-        script_url_se=script_url_se,
-        location_n=location_n,
-        location_se=location_se,
+        # script_url_n=script_url_n,
+        # script_url_se=script_url_se,
+        # location_n=location_n,
+        # location_se=location_se,
         script_url_nl_n=script_url_nl_n,
         script_url_nl_se=script_url_nl_se,
         location_nl_n=location_nl_n,
